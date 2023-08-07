203c1e0f415544782475d71b68a0e3cfc65342c3
Henree001 <okaforhenry779@gmail.com>
2023-08-07 13:57:02 +0100

    solutions to task

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+    const allemployees = {
+        allEmployees: {
+           ...employeesList,
+        },
+        getNumberOfDepartments() {
+            return Object.keys(this.allEmployees).length;
+        },
+      };
+    return allemployees;
+}
