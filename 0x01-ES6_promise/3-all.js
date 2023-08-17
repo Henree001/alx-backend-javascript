1a36d36b29f6f16d6cb70e4f4b92180d94e91c3d
Henree001 <okaforhenry779@gmail.com>
2023-08-17 18:00:04 +0100

    fixed indentation

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from '../utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then(([Image, User]) => {
-      console.log(`${Image.body} ${User.firstName} ${User.lastName}`);
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
