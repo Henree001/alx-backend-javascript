37704fb2dca4b5b6762cb9ffbb7ab45b2ed772b8
Henree001 <okaforhenry779@gmail.com>
2023-09-01 11:07:04 +0100

    Fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function (set, str) {
+  if (str === null || typeof str !== 'string') {
+    return '';
+  }
   let newString = [];
   for (const item of set) {
     if (item.startsWith(str) && (str.length !== 0)) {
-      newString.push(item.slice(str.length));
+      newString.push(item.slice(str.length - 1));
     }
   }
   newString = newString.join('-');
