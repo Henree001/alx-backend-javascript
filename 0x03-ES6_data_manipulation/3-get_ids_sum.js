84cfb2fb4b0ca0264fc8a7eaa7263c7fc7a31b93
Henree001 <okaforhenry779@gmail.com>
2023-09-01 11:27:08 +0100

    Fixed task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(studentlist) {
-  const sumOfId = studentlist.reduce((sum, student) => sum + student.id);
+  const sumOfId = studentlist.reduce((sum, student) => sum + student.id, 0);
   return sumOfId;
 }
