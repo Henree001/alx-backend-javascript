f0d374d82b790da2919c6bcc9f296726f8e0c4c1
Henree001 <okaforhenry779@gmail.com>
2024-02-25 15:47:17 +0100

    initial push

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('integration Testing', () => {
+  describe('gET /', () => {
+    it('code: 200 | Body: Welcome to the payment system', () => new Promise((done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    }));
+  });
+});
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+Learning how to write unitests in javascript
